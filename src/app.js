e99cced57840cb3ba59629ffd56a680d78f6299d
Moises Velasquez <mvelasquez@pictoric.com>
2022-02-03 10:52:04 -0500

    return json with info package

--- src/app.js
+++ src/app.js
@@ -1,8 +1,20 @@
 import express from 'express';
 import morgan from 'morgan';
 
+import pkg from '../package.json';
+
 const app = express();
 
 app.use(morgan('dev'));
+
+app.set('pkg', pkg);
+
+app.get('/', (req, res) => {
+	res.json({
+		name: app.get('pkg').name,
+		description: app.get('pkg').description,
+		author: app.get('pkg').author
+	});
+});
 
 export default app;
