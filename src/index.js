33c84e0fd9cb76dc123818b996966bf092f72ccb
Moises Velasquez <mvelasquez@pictoric.com>
2022-02-03 10:31:43 -0500

    created file configs project

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
-import express from 'express';
+import app from './app';
 
-const app = express();
-app.listen(3000);
+app.listen(4000);
 
-console.log('Server listen on port: ', 3000);
+console.log('Server listen on port: ', 4000);
